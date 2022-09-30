d5505af9aeecdfc5e66a1d2735132d65f3a367d9
Catherine :) <43099514+catherinesyeh@users.noreply.github.com>
2022-09-29 23:02:49 -0400

    Delete firebaseConfig.js

--- src/lib/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBTBLZsjR9nfMXP66CaeJyRy7ze7fKEOC0",
-  authDomain: "sveltekit-todos-f44ce.firebaseapp.com",
-  projectId: "sveltekit-todos-f44ce",
-  storageBucket: "sveltekit-todos-f44ce.appspot.com",
-  messagingSenderId: "921183474915",
-  appId: "1:921183474915:web:0436bb4b08ddcf0f1061d6"
-};
